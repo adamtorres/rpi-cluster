80b74bcad32eb773ec3264b919d81be296bf9752
Adam Torres <adamprogrammer@gmail.com>
2020-08-15 01:05:40 -0600

    uncommented the lines that do the work in the rename script. Commented some echos in the resize script.

--- manual/rename-pi.sh
+++ manual/rename-pi.sh
@@ -3,6 +3,5 @@
 # Generates a hostname that is "pi-" followed by the last 6 characters of the wifi MAC addres.
 NEW_HOSTNAME="pi-$(ip a show wlan0 | grep 'link/ether' | grep -o '..:..:.. ' | tr '[:lower:]' '[:upper:]' | tr -d '[:punct:]' | tr -d '[:blank:]')"
 echo "Renaming pi from '$(hostname)' to '$NEW_HOSTNAME'"
-# sudo sed -i.bak -e "s/.*/$NEW_HOSTNAME/" /etc/hostname
-# sudo sed -i.bak -e "s/127\.0\.1\.1.*/127.0.1.1\t\t$NEW_HOSTNAME $NEW_HOSTNAME.local/" /etc/hosts
-
+sudo sed -i.bak -e "s/.*/$NEW_HOSTNAME/" /etc/hostname
+sudo sed -i.bak -e "s/127\.0\.1\.1.*/127.0.1.1\t\t$NEW_HOSTNAME $NEW_HOSTNAME.local/" /etc/hosts
